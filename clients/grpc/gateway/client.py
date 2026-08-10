0560524248c040485ac097b15c81844a7d1ddaaf
Viktar Vasileuski <victor.vasilev1991@gmail.com>
2026-08-10 12:25:03 +0300

    use settings across the project

--- clients/grpc/gateway/client.py
+++ clients/grpc/gateway/client.py
@@ -2,12 +2,13 @@ from grpc import Channel, insecure_channel, intercept_channel
 from locust.env import Environment
 
 from clients.grpc.interceptors.locust_interceptor import LocustInterceptor
+from config import settings
 
 
 def build_gateway_grpc_client() -> Channel:
-    return insecure_channel('192.168.56.2:9003')
+    return insecure_channel(settings.gateway_grpc_client.client_url)
 
 
 def build_gateway_locust_grpc_client(environment: Environment) -> Channel:
-    channel = insecure_channel('192.168.56.2:9003')
+    channel = insecure_channel(settings.gateway_grpc_client.client_url)
     return intercept_channel(channel, LocustInterceptor(environment))
